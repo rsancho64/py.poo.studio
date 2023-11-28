7173a8129feca48b94798bce554251db9b9bac25
ramon sancho <rsancho@iesrioarba.es>
2023-11-28 01:28:22 +0100

    Update Employee.py

--- class/Employee.py
+++ class/Employee.py
@@ -19,6 +19,9 @@ class Employee:
         """vs __str__"""
         print('Employee:', self.name, self.salary, "working @", self.company_name)
 
+    def __del__(self):
+        print(f"adios mundo cruel")
+
     # class method
     @classmethod
     def modify_company_name(cls, newName):
@@ -32,21 +35,32 @@ class Employee:
 if __name__ == "__main__":
 
     # create first object
+
+    # emp0 = new Employee ver como se usa new
+
     emp1 = Employee("Harry", 12000)
-    print(emp1)
-    emp1.show()
-    
-    # create second object
     emp2 = Employee("Emma", 10000)
+    print(emp1)
     print(emp2)
-    emp2.show() 
-       
+
+    print()
+
     emp2.company_name = "SPACEX"
     print(emp1)
     print(emp2)
-    
+
+    print()
+
     Employee.modify_company_name("Roscosmos")
     print(emp1)
     print(emp2)
-    emp3 = Employee("Vasili", 10000)    
-    print(emp3)    
+
+    print()
+
+    emp3 = Employee("Vasili", 1000)    
+    print(emp1)
+    print(emp2)
+    print(emp3)
+
+    # del emp3
+    # print(emp3)
